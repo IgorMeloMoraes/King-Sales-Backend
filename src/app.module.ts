dca7941030bda22620190cf54d42a4a3bcb43997
IgorMeloMoraes <igor-batista@outlook.com.br>
2025-05-16 19:21:34 -0300

    Configuracao do banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  // Configuração do ConfigModule para carregar variáveis de ambiente
+  // O ConfigModule irá carregar as variáveis de ambiente dos arquivos especificados
+  // Agora temos o nosso environment configurado para desenvolvimento
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+
+    // Configuração do TypeOrmModule para conectar ao banco de dados
+    // As variáveis de ambiente são carregadas do arquivo .env
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
